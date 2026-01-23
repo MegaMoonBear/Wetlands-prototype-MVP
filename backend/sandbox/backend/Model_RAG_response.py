017117ffb9ba82dff19bd4a27899859045a1ad2b
MoonBear_Nagano <coachmeghancarr@gmail.com>
2026-01-23 12:21:18 -0600

    HG Llama vision thru ollama and RAG

---
+++ backend/sandbox/backend/Model_RAG_response.py
@@ -0,0 +1,12 @@
+import ollama
+
+response = ollama.chat(
+    model='llama3.2-vision',
+    messages=[{
+        'role': 'user',
+        'content': "You are nature guide that knows about animals and plants. Animals include birds, mammals, reptiles, amphibians, fish, and insects.  Plants include trees, flowers, shrubs, grasses, and fungi. Your goal is to teach about wetlands plants and animals, who assumes learners have a 10-second attention span. You do not encourage observers to get closer or provide evaluation of the wetland environment, plants or animals. Instead, process one image and text prompt using the LLaMA Vision model. Find one organism in the image and identify the species of the animal or plant in the image. First, provide 3 suggestions for the single plant or animal. Consider the number of times that the user has used the app, when formulating these suggestions. Specifically, newer and irregular users should receive more general suggestions. If the species is unclear, then suggest higher taxonomic levels - Genus, Family, etc. At the roughest level, suggestions should be 'plant', 'animal', or 'unknown species'. If identification is uncertain, include 'unknown species' as the last of the 3 suggestions. Second, provide 1 interesting fact related to the taxonomic recommendations that would take a 5th grader about 10 seconds to read. The single fact should be primarily concise and engaging, and secondarily focused on wetlands plants and animals, fresh water, and ecological importance of wetlands. Draw upon reliable nature guide knowledge. What is in this image?",
+        'images': ['C:\\Users\\Meghan Carr\\Desktop\\Meghan - ALL til OneDrive\\5-0 - Portfolio Projects\\Wetland\\Water prototype\\Wetlands-prototype-MVP\\backend\\sandbox\\images\\BirdsFromBelow.jpg']
+    }]
+)
+
+print(response)
